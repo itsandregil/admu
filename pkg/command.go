12082dbd6b7d04859f443d29dff043e42ff663b4
Andre Gil <thomasgil913@gmail.com>
2026-04-20 23:08:53 -0500

    Update add command

--- pkg/command.go
+++ pkg/command.go
@@ -6,14 +6,14 @@ import (
 )
 
 func RunCommand(cmd string, args ...string) {
-	arguments := getArguments(cmd, args)
+	arguments := parseArguments(cmd, args)
 	command := exec.Command("sudo", arguments...)
 	if err := command.Run(); err != nil {
 		fmt.Printf("command failed: %v\n", err)
 	}
 }
 
-func getArguments(cmd string, args []string) []string {
+func parseArguments(cmd string, args []string) []string {
 	arguments := []string{}
 	arguments = append(arguments, cmd)
 	arguments = append(arguments, args...)
